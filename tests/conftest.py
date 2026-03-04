99a6608dbfd81889725b51beb17ddbe2fb047ab4
Alexis Placet <2400067+Alex-PLACET@users.noreply.github.com>
2026-03-04 22:08:17 +0100

    Revert "Update tests/conftest.py"
    This reverts commit c6bd73ad1ae855f4acc11657da06459e3b046012.

--- tests/conftest.py
+++ tests/conftest.py
@@ -82,32 +82,22 @@ def delete_arcticdb(init_arcticdb):
     gc.collect()
 
     # Windows may keep LMDB lock files briefly; retry deletion a few times.
-    last_exc = None
     for _ in range(10):
         try:
             ac.delete_library(lib_name)
-            last_exc = None
             break
-        except Exception as exc:
-            last_exc = exc
+        except Exception:
             gc.collect()
             time.sleep(0.1)
-    if last_exc is not None:
-        raise last_exc
 
     init_arcticdb.pop("ac", None)
     del ac
     gc.collect()
 
-    last_exc = None
     for _ in range(10):
         try:
             shutil.rmtree(lmdb_path)
-            last_exc = None
             break
-        except Exception as exc:
-            last_exc = exc
+        except Exception:
             gc.collect()
             time.sleep(0.1)
-    if last_exc is not None:
-        raise last_exc
